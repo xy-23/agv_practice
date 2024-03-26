675fa84d67ca6918929d185e535d8ecdac410dbb
IcyCarbon <Exhibkoro@gmail.com>
2024-03-26 13:14:54 +0800

    add hex to laserscan

--- agv_lidar/agv_lidar/json_transformer.py
+++ agv_lidar/agv_lidar/json_transformer.py
@@ -1,16 +1,20 @@
 import json
 
+# use json to analyse data
 def Json_transformer(data):
     
-    # Parse the data into structured format
     parsed_data = {
         "header": data[:16],
         "text_info": data[16:44],
-        "scan_data": [data[i:i+8] for i in range(44, len(data)-26, 8)],
+        "scan_data": [data[i:i+4] for i in range(44, len(data)-26, 8)],
+        "re_data": [data[i+4:i+8] for i in range(44, len(data)-26, 8)],
         "timestamp": data[-22:-2],
         "checksum": data[-2:]
     }
 
-    # Convert to JSON
     json_data = json.dumps(parsed_data, indent=4)
-    return json_data
+    dict_data = json.load(json_data)
+    range_list = list(map(lambda x: int(x, 16), dict_data["scan_data"]))
+    re_list = list(map(lambda x: int(x, 16), dict_data["re_data"]))
+
+    return json_data, [range_list, re_list]
