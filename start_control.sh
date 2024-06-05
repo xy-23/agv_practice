41bbd5578dedfbb62204bf9c2e15cf9164c35dfb
XiaoYoung <xy_23@outlook.com>
2024-06-05 18:12:45 +0800

    update start.sh

--- start_control.sh
+++ start_control.sh
@@ -1,7 +1,8 @@
+cd /agv_ws
 source install/setup.bash
 
 export ROS_DOMAIN_ID=1
 export FASTRTPS_DEFAULT_PROFILES_FILE=/agv_ws/src/agv_practice/whitelist.xml 
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp 
 
-ros2 launch agv_hw test.launch.py
+ros2 launch agv_hw control.launch.py
