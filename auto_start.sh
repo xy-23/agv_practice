e731c65af922dfa5c96966076dc3815c5208077b
XiaoYoung <xy_23@outlook.com>
2024-06-30 19:07:57 +0800

    update auto_start

--- auto_start.sh
+++ auto_start.sh
@@ -2,6 +2,18 @@ sleep 1
 
 docker start agv
 
-screen \
-    -dmS control \
-    docker exec -it agv bash -c '/agv_ws/src/agv_practice/start_control.sh'
+sleep 1
+
+screen -dmS control docker exec -it agv bash -c '/agv_ws/src/agv_practice/start_control.sh'
+
+sleep 1
+
+screen -dmS lidar docker exec -it agv bash -c '/agv_ws/src/agv_practice/start_lidar.sh'
+
+sleep 1
+
+screen -dmS amcl docker exec -it agv bash -c '/agv_ws/src/agv_practice/start_amcl.sh'
+
+sleep 1
+
+screen -dmS nav docker exec -it agv bash -c '/agv_ws/src/agv_practice/start_nav.sh'
