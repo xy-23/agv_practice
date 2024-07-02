bedcf8044df134f47e3213c3d3d7309a28aecffa
XiaoYoung <xy_23@outlook.com>
2024-07-02 17:33:00 +0800

    update map and nav2_params

--- start_amcl.sh
+++ start_amcl.sh
@@ -5,4 +5,6 @@ export ROS_DOMAIN_ID=1
 export FASTRTPS_DEFAULT_PROFILES_FILE=/agv_ws/src/agv_practice/whitelist.xml 
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp 
 
-ros2 launch nav2_bringup localization_launch.py params_file:=src/agv_practice/agv_nav/config/nav2_params.yaml map:=src/agv_practice/agv_nav/map/lab2.yaml 
+ros2 launch nav2_bringup localization_launch.py \
+    map:=src/agv_practice/agv_nav/map/lab102-2.yaml \
+    params_file:=src/agv_practice/agv_nav/config/nav2_params.yaml
--- start_nav.sh
+++ start_nav.sh
@@ -5,4 +5,5 @@ export ROS_DOMAIN_ID=1
 export FASTRTPS_DEFAULT_PROFILES_FILE=/agv_ws/src/agv_practice/whitelist.xml 
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp 
 
-ros2 launch nav2_bringup navigation_launch.py
+ros2 launch nav2_bringup navigation_launch.py \
+    params_file:=src/agv_practice/agv_nav/config/nav2_params.yaml
