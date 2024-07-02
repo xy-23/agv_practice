12b1b85f3624bc6c69b2ab59f6f4fece0d861e6b
XiaoYoung <xy_23@outlook.com>
2024-07-02 17:42:29 +0800

    update agv_nav

--- start_rviz.sh
+++ start_rviz.sh
@@ -4,4 +4,4 @@ source /opt/ros/humble/setup.bash
 
 export ROS_DOMAIN_ID=1
 
-rvizz -d ~/Desktop/agv_ws/src/agv_practice/agv_hw/rviz/test.rviz
+rvizz -d ~/Desktop/agv_ws/src/agv_practice/agv_nav/config/nav.rviz
